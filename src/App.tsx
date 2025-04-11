913977af15c80f9ae73c793e176e91ab0aa06a45
zana-abdollahpour <zana.ap4@gmail.com>
2025-04-11 13:46:35 +0330

    add VerticalCarousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { Children } from "react";
+
+function VerticalCarousel({
+  children,
+}: {
+  children: React.JSX.Element | React.JSX.Element[];
+}) {
+  return Children.map(children, (child: React.JSX.Element) => (
+    <div className="relative m-5 flex w-full flex-col items-center justify-center bg-amber-600 py-44">
+      <h1 className="absolute top-1 left-2">
+        {child.type.name || "Native Element"}
+      </h1>
+      {child}
+    </div>
+  ));
+}
+
 export default function App() {
-  return <div className="container">initial commit</div>;
+  return (
+    <div className="mx-auto flex max-w-6xl flex-col items-center justify-center px-4">
+      <VerticalCarousel>
+        <div>Test</div>
+      </VerticalCarousel>
+    </div>
+  );
 }
